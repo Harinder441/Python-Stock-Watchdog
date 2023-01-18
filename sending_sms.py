dc4b5c5302f6021f975428843fc096d98e90676f
Harinder Singh <99085182+Harinder441@users.noreply.github.com>
2023-01-18 11:03:30 +0530

    Update sending_sms.py

--- sending_sms.py
+++ sending_sms.py
@@ -4,14 +4,14 @@ from twilio.rest import Client
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = 'AC6cfe0cdea4775d8cadfd4e2023228325'
-auth_token = '9950f4656985fc96fdac23b7ea65e520'
+account_sid =yOUR_sid
+auth_token = YOUR_TOKEN
 client = Client(account_sid, auth_token)
 
 def send_massage(massage:str,to_num:str):
     message = client.messages.create(
                                   body=massage,
-                                  from_='+18059024520',
+                                  from_=YOUR_NUMBER,
                                   to=to_num,
                           )
     print(message.status)
